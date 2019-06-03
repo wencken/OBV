8a8e4d890961c7182eb8d70c90fcaff7bb9a9981
Wencke Nuyt <wencke.nuyt@student.howest.be>
2019-06-03 23:43:13 +0200

    stories home

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import { inject, observer } from "mobx-react";
 import PageHeader from "../components/PageHeader";
 import Navigation from "../components/Navigation";
 
-const Home = () => {
+const Home = ({ storyStore }) => {
+  const { stories } = storyStore;
+
   return (
     <>
       <PageHeader title={`De Derde Boodschap`} />
       <Navigation />
+      {stories.length > 0 ? (
+        <ul>
+          {stories.map(story => (
+            <p> {story.description}</p>
+          ))}
+        </ul>
+      ) : (
+        <p>No stories</p>
+      )}
     </>
   );
 };
 
-export default Home;
+export default inject(`storyStore`)(observer(Home));
