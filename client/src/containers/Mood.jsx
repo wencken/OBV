205fa25eb424194f84b4351cf8fef339965eb97f
Wencke Nuyt <wencke.nuyt@student.howest.be>
2019-06-04 12:52:57 +0200

    statistieken

--- client/src/containers/Mood.jsx
+++ client/src/containers/Mood.jsx
@@ -1,14 +1,25 @@
 import React from "react";
+import { inject, observer } from "mobx-react";
 import PageHeader from "../components/PageHeader";
 import Navigation from "../components/Navigation";
 
-const Mood = () => {
+const Mood = ({ moodStore, storyStore }) => {
+  const { moods } = moodStore;
+  // const moodCat = storyStore.story.mood;
+  // console.log(moodCat);
+
   return (
     <>
       <PageHeader title={`How are we doing?`} />
       <Navigation />
+
+      <div>
+        {moods.map(mood => (
+          <p>{mood.name}</p>
+        ))}
+      </div>
     </>
   );
 };
 
-export default Mood;
+export default inject(`storyStore`, `moodStore`)(observer(Mood));
