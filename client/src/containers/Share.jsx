5b985a961d9fb8a6d128e38656b722486306fdd7
Wencke Nuyt <wencke.nuyt@student.howest.be>
2019-06-13 11:31:05 +0200

    question share

--- client/src/containers/Share.jsx
+++ client/src/containers/Share.jsx
@@ -92,15 +92,126 @@ const Share = ({ uiStore, moodStore, storyStore, emailStore, history }) => {
                     // checked={this.state.selected === "Ghent"}
                     onChange={setMood}
                   />
-                  {mood.name}
+                  <span
+                    className={
+                      mood.name
+                        ? mood.name === "happy"
+                          ? ` bg_yellow`
+                          : mood.name === "sad"
+                          ? ` bg_blue `
+                          : ` bg_pink `
+                        : "visually-hidden"
+                    }
+                  >
+                    {mood.name}
+                  </span>
                 </label>
               </li>
             ))}
           </ul>
         )}
 
-        <label htmlFor="description" className={styles.reverse}>
-          Question
+        <ul>
+          {moodStore.moods.map(mood => (
+            <li
+              key={mood.id}
+              className={
+                mood.name
+                  ? moodInput === "happy"
+                    ? `${styles.reverse}`
+                    : moodInput === "sad"
+                    ? `${styles.reverse}`
+                    : `${styles.reverse}`
+                  : "visually-hidden"
+              }
+            >
+              <label
+                htmlFor="description"
+                className={
+                  mood.name
+                    ? mood.name === "happy"
+                      ? `${styles.reverse}`
+                      : mood.name === "sad"
+                      ? `${styles.reverse}`
+                      : `${styles.reverse}`
+                    : "visually-hidden"
+                }
+              >
+                What have you experienced lately that made you feel very{" "}
+                <span
+                  className={
+                    mood.name
+                      ? mood.name === "happy"
+                        ? ` bg_yellow text_underline`
+                        : mood.name === "sad"
+                        ? ` bg_blue text_underline`
+                        : ` bg_pink text_underline`
+                      : "visually-hidden"
+                  }
+                >
+                  {mood.name}
+                </span>
+                ?
+                <textarea
+                  className={
+                    mood.name
+                      ? mood.name === "happy"
+                        ? `${styles.textarea_yellow} ${styles.textarea} `
+                        : mood.name === "sad"
+                        ? `${styles.textarea_blue} ${styles.textarea} `
+                        : `${styles.textarea_pink} ${styles.textarea} `
+                      : styles.textarea
+                  }
+                  type="textarea"
+                  name="description"
+                  id="description"
+                  placeholder="Tell us your story..."
+                  ref={descriptionInput}
+                  required
+                />
+              </label>
+            </li>
+          ))}
+        </ul>
+        {/* <label htmlFor="description" className={styles.reverse}>
+          <ul>
+            <li
+              className={
+                currentMood
+                  ? currentMood === "happy"
+                    ? "color_black"
+                    : "visually-hidden"
+                  : "visually-hidden"
+              }
+            >
+              What have you experienced lately that made you feel very{" "}
+              <span className={"text_underline bg_yellow"}>{mood.name}</span>?
+            </li>
+            <li
+              className={
+                currentMood
+                  ? currentMood === "sad"
+                    ? "color_black"
+                    : "visually-hidden"
+                  : "visually-hidden"
+              }
+            >
+              What have you experienced lately that made you feel very{" "}
+              <span className={"text_underline bg_blue color_white"}>sad</span>?
+            </li>
+            <li
+              className={
+                currentMood
+                  ? currentMood === "mad"
+                    ? ""
+                    : "visually-hidden"
+                  : "visually-hidden"
+              }
+            >
+              What have you experienced lately that made you feel very{" "}
+              <span className={"text_underline bg_pink color_white"}>mad</span>?
+            </li>
+          </ul>
           <textarea
             className={
               currentMood
@@ -118,7 +229,7 @@ const Share = ({ uiStore, moodStore, storyStore, emailStore, history }) => {
             ref={descriptionInput}
             required
           />
-        </label>
+        </label> */}
         <CheckBox emailInput={emailInput} />
         <input type="submit" value="Share your story" />
       </form>
