60fa402310c3b1ebef0b91c853ca998a619f3455
ThomasHabets99 <thomas.habets@student.howest.be>
2019-06-14 06:04:37 +0200

    stories fixed

--- client/src/containers/Home.jsx
+++ client/src/containers/Home.jsx
@@ -15,13 +15,13 @@ const Home = ({ uiStore, storyStore, moodStore }) => {
   const { moods, currentMood } = moodStore;
   console.log(moods);
 
-  const toggleStories = event => {
-    if (event.target.classList.contains("toggle_animation")) {
-      event.target.classList.remove("toggle_animation");
-    } else {
-      event.target.classList.add("toggle_animation");
-    }
-  };
+  // const toggleStories = event => {
+  //   if (event.target.classList.contains("toggle_animation")) {
+  //     event.target.classList.remove("toggle_animation");
+  //   } else {
+  //     event.target.classList.add("toggle_animation");
+  //   }
+  // };
 
   const { currentCity } = uiStore;
 
@@ -45,7 +45,7 @@ const Home = ({ uiStore, storyStore, moodStore }) => {
     <>
       <div className={styles.container_main}>
         <div className={"container_switch"}>
-          <div>
+          <div className={styles.container_wrapper}>
             <article
               className={
                 currentMood
@@ -127,7 +127,7 @@ const Home = ({ uiStore, storyStore, moodStore }) => {
           className={`
         ${styles.container_stories}
         `}
-          onClick={e => toggleStories(e)}
+          // onClick={e => toggleStories(e)}
         >
           <button className={`${styles.toggle}`}>VVVVVVVVV</button>
           <div
